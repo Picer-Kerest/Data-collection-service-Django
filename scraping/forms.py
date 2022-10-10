691b29781a5d4caaf25862f48835a0bee8a6b028
Jailyss Neust <kabakovmaksim76@gmail.com>
2022-10-10 20:28:29 +0300

    New functional

---
+++ scraping/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import City, Language, Vacancy
+
+class FindForm(forms.Form):
+    city = forms.ModelChoiceField(label='City', queryset=City.objects.all(), to_field_name='slug', required=False,
+                                  widget=forms.Select(attrs={'class': 'form-control js-example-basic-single'}))
+    language = forms.ModelChoiceField(label='Programming language', queryset=Language.objects.all(), to_field_name='slug', required=False,
+                                      widget=forms.Select(attrs={'class': 'form-control js-example-basic-single'}))
+
--- scraping_service/urls.py
+++ scraping_service/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from scraping import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
+    path('home/', views.home_view, name='home'),
+    path('', views.start),
 ]
