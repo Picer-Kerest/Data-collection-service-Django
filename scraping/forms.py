090d89416ac8bfb1ea42128af8c1739713df21fd
Jailyss Neust <kabakovmaksim76@gmail.com>
2022-10-12 00:04:25 +0300

    Overriding the base user

--- scraping/forms.py
+++ scraping/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import City, Language, Vacancy
+from .models import City, Language
 
 class FindForm(forms.Form):
     city = forms.ModelChoiceField(label='City', queryset=City.objects.all(), to_field_name='slug', required=False,
