87313e832db83e1c637eb9e63b64dabd47ea6062
Jailyss Neust <kabakovmaksim76@gmail.com>
2022-11-17 00:40:46 +0300

    ordering in admin panel

--- scraping/admin.py
+++ scraping/admin.py
@@ -3,7 +3,11 @@ from .models import City, Language, Vacancy, Error, Url
 
 admin.site.register(City)
 admin.site.register(Language)
-admin.site.register(Vacancy)
+# admin.site.register(Vacancy)
 admin.site.register(Error)
 admin.site.register(Url)
 
+
+@admin.register(Vacancy)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'language', 'city', 'timestamp']
